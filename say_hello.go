c23b8dc0ee4bdaac37f36c8c7f1517c3496bb92e
adhityaf <adhityafebhiakbar@gmail.com>
2022-02-21 09:43:37 +0700

    add age as second parameter

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello(name string) string {
-	return "Hello " + name
+func SayHello(name string, age string) string {
+	return "Hello " + name + ", i am " + age + " years old."
 }
